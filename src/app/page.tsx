318335b9bc494990d701faf218878f7cd11a77ae
Victor Campos <victor.htp@hotmail.com>
2023-10-25 20:52:24 -0300

    feat: defining product type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
-export default function Home() {
+import { ProductType } from "@/types/ProductType"
+
+async function getProducts() {
+  const url = "https://fakestoreapi.com/products"
+  try {
+    const res = await fetch(url)
+    return res.json()
+  } catch (error) {}
+}
+
+export default async function Home() {
+  const products = await getProducts()
+  console.log(products)
   return (
     <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
-        <div className="bg-white">prod 1</div>
-        <div className="bg-white">prod 1</div>
-        <div className="bg-white">prod 1</div>
-        <div className="bg-white">prod 1</div>
+        {products.map((product: ProductType) => (
+          <div key={product.id}>{product.title}</div>
+        ))}
       </div>
     </div>
   )
