20de2060e6d77040e4be46428bb9b556cc741348
Victor Campos <victor.htp@hotmail.com>
2023-10-25 20:44:07 -0300

    feat: Products list pt.1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
-    <div>
-      NEXT COMMERCE
+    <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
+        <div className="bg-white">prod 1</div>
+        <div className="bg-white">prod 1</div>
+        <div className="bg-white">prod 1</div>
+        <div className="bg-white">prod 1</div>
+      </div>
     </div>
   )
 }
