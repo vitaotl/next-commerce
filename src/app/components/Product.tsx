c8ad0f36c9e65978847312d19213d6af9b4b3b69
Victor Campos <victor.htp@hotmail.com>
2023-10-25 21:02:32 -0300

    feat: Add product component

---
+++ src/app/components/Product.tsx
@@ -0,0 +1,18 @@
+import { ProductType } from "@/types/ProductType"
+import React from "react"
+
+type ProductProps = {
+  product: ProductType
+}
+
+const Product = ({ product }: ProductProps) => {
+  return (
+    <div className="flex flex-col shadow-lg h-96 bg-slate-800 p-5 text-gray-300">
+      <div className="relative max-h-72 flex-1">IMG</div>
+      <div className="flex justify-between font-bold my-3">{product.title}</div>
+      <button className="rounded-md bg-teal-600 text-white px-3.5 py-2.5 text-sm text-center">Adicionar ao carrinho</button>
+    </div>
+  )
+}
+
+export default Product
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,27 @@
 import { ProductType } from "@/types/ProductType"
+import Product from "./components/Product"
 
 async function getProducts() {
   const url = "https://fakestoreapi.com/products"
   try {
     const res = await fetch(url)
-    return res.json()
-  } catch (error) {}
+    if (res.ok) {
+      return res.json()
+    }
+    throw new Error(`Erro na solicitação: ${res.status}`)
+  } catch (error) {
+    console.error(error)
+    // Você pode tratar o erro de outras maneiras, como retornar um objeto de erro personalizado.
+  }
 }
 
 export default async function Home() {
   const products = await getProducts()
-  console.log(products)
   return (
     <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
         {products.map((product: ProductType) => (
-          <div key={product.id}>{product.title}</div>
+          <Product key={product.id} product={product} />
         ))}
       </div>
     </div>
